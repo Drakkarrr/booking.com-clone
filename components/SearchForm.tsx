b3ae5a00d1a20cbd10f1aa16e43837169d2ab959
drakkarrr/resume <drakkar.alpha00@gmail.com>
2024-01-02 06:32:57 +0800

    constructed handle submit on form

--- components/SearchForm.tsx
+++ components/SearchForm.tsx
@@ -5,6 +5,18 @@ import { zodResolver } from '@hookform/resolvers/zod';
 import { useForm } from 'react-hook-form';
 import { useRouter } from 'next/navigation';
 
+import { Button } from '@/components/ui/button';
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from '@/components/ui/form';
+import { Input } from '@/components/ui/input';
+
 export const formSchema = z.object({
   location: z.string().min(2).max(50),
   dates: z.object({
