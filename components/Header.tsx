bb61fc9af2cfb8b4a054b03f4a493b2ee219ce6b
drakkarrr/resume <drakkar.alpha00@gmail.com>
2024-01-01 08:34:15 +0800

    title chaned

--- components/Header.tsx
+++ components/Header.tsx
@@ -48,7 +48,7 @@ const Header = () => {
         aria-label='Global'>
         <div className='flex lg:flex-1'>
           <Link href='/' className='-m-1.5 p-1.5'>
-            <span className='sr-only'>Booking.com</span>
+            <span className='sr-only'>Booking.com clone</span>
             <img
               className='w-auto h-12'
               src='https://static1.squarespace.com/static/5bde0f00c3c16aa95581e2e2/62b4cb1add9d257dd43bb03d/62b653fedc7c895918d19b24/1656116254983/booking+logo+white.png?format=1500w'
