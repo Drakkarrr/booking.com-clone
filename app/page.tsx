fed736de6ac1cf1444e4fad8cc55039d50cc9a9d
drakkarrr/resume <drakkar.alpha00@gmail.com>
2024-01-01 08:49:42 +0800

    Built basic compoent for homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,21 @@
 export default function Home() {
-  return <header className='bg-[#3e73c3]'>Booking</header>;
+  return (
+    <main className='bg-[#013B94]'>
+      <section className='p-6 mx-auto max-w-7xl'>
+        <h2 className='text-5xl font-bold text-white'>Find your Next Stay</h2>
+        <h3 className='py-5 text-xl text-white'>
+          Search low prices on hotels, homes and much more!
+        </h3>
+      </section>
+
+      <section className='p-6 mx-auto mt-10 bg-white rounded-t-lg max-w-7xl'>
+        <div className='pt-5'>
+          <h3 className='text-xl font-bold'>Trending Destinations</h3>
+          <p className='font-light'>
+            Most popular choices for travellers from around the world
+          </p>
+        </div>
+      </section>
+    </main>
+  );
 }
