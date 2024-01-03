13395b984c7f6611324fefb962119e0798c8f7d6
drakkarrr/resume <drakkar.alpha00@gmail.com>
2024-01-03 14:35:38 +0800

    adds types for props

--- app/search/page.tsx
+++ app/search/page.tsx
@@ -1,6 +1,19 @@
 import React from 'react';
 
-const SearchPage = (props) => {
+type Props = {
+  searchParams: SearchParams;
+};
+
+export type SearchParams = {
+  url: URL;
+  group_adults: string;
+  group_children: string;
+  no_rooms: string;
+  checkin: string;
+  checkout: string;
+};
+
+const SearchPage = (props: Props) => {
   console.log(props);
 
   return <div>SearchPageee</div>;
