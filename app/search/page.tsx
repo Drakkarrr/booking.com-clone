a6cb5eeec5f697ed8ab721c5481fa0c3d4ae3e74
drakkarrr/resume <drakkar.alpha00@gmail.com>
2024-01-03 14:38:27 +0800

    displays params check data

--- app/search/page.tsx
+++ app/search/page.tsx
@@ -13,10 +13,19 @@ export type SearchParams = {
   checkout: string;
 };
 
-const SearchPage = (props: Props) => {
-  console.log(props);
+const SearchPage = ({ searchParams }: Props) => {
+  console.log(searchParams);
 
-  return <div>SearchPageee</div>;
+  return (
+    <div>
+      SearchPageee
+      <p>{searchParams.group_adults}</p>
+      <p>{searchParams.group_children}</p>
+      <p>{searchParams.no_rooms}</p>
+      <p>{searchParams.checkin}</p>
+      <p>{searchParams.checkout}</p>
+    </div>
+  );
 };
 
 export default SearchPage;
